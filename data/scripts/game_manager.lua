14f7d27fb1369e8fafb2817f27e33338b9ee88c5
GGuy22 <greenguyon@yahoo.com>
2015-01-20 15:03:11 -0700

    initial setup

---
+++ data/scripts/game_manager.lua
@@ -0,0 +1,59 @@
+-- Script that creates a game ready to be played.
+
+-- Usage:
+-- local game_manager = require("scripts/game_manager")
+-- local game = game_manager:create("savegame_file_name")
+-- game:start()
+
+local dialog_box_manager = require("scripts/dialog_box")
+
+local game_manager = {}
+
+-- Sets initial values for a new savegame of this quest.
+local function initialize_new_savegame(game)
+  game:set_starting_location("debug")
+  game:set_max_money(100)
+  game:set_max_life(12)
+  game:set_life(game:get_max_life())
+end
+
+-- Creates a game ready to be played.
+function game_manager:create(file)
+
+  -- Create the game (but do not start it).
+  local exists = sol.game.exists(file)
+  local game = sol.game.load(file)
+  if not exists then
+    -- This is a new savegame file.
+    initialize_new_savegame(game)
+  end
+ 
+  local dialog_box
+
+  -- Function called when the player runs this game.
+  function game:on_started()
+
+    dialog_box = dialog_box_manager:create(game)
+  end
+
+  -- Function called when the game stops.
+  function game:on_finished()
+
+    dialog_box:quit()
+    dialog_box = nil
+  end
+
+  function game:on_paused()
+
+    game:start_dialog("pause.save", function(answer)
+      if answer == 2 then
+        game:save()
+      end
+      game:set_paused(false)
+    end)
+  end
+
+  return game
+end
+
+return game_manager
